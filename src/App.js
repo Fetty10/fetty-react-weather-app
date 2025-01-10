361d6c1ad2cf0d318b848518b4b894073393767b
Faith Osabutey <osabuteyfaith@gmail.com>
2025-01-10 19:28:29 +0000

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a href="https://github.com/Fetty10"> Fetty</a> and{" "}
+          <a href="https://github.com/Fetty10/fetty-react-weather-app">
+            {" "}
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
 }
 
 export default App;
