087c75d0cb79b6776720ea00a2e337c9bcc21ae1
Faith Osabutey <osabuteyfaith@gmail.com>
2025-01-12 00:26:20 +0000

    Made chages to the footer

--- src/App.js
+++ src/App.js
@@ -11,10 +11,14 @@ function App() {
 
         <footer>
           This project was coded by{" "}
-          <a href="https://github.com/Fetty10"> Fetty</a> and{" "}
+          <a href="https://github.com/Fetty10"> Fetty</a> and it is{" "}
           <a href="https://github.com/Fetty10/fetty-react-weather-app">
             {" "}
             open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a href="https://react-weather-app-fetty.netlify.app/">
+            hosted on Netlify
           </a>
         </footer>
       </div>
